2856f81bd29dff32d12b7fbce2a36d02f717bdf3
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-04-20 22:56:21 +0300

    Create classes

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,15 @@
+var Customer = /** @class */ (function () {
+    function Customer(theFirst, theLast) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
+    return Customer;
+}());
+// Let's create an instance
+/*let myCustomer = new Customer();
+myCustomer.firstName = "Martin";
+myCustomer.lastName = "Dixon";
+*/
+var myCustomer = new Customer("Martin", "Dixon");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
